63d5ec69f6da87e6f98214396f4f79ad0a18c056
my <my@aaa.com>
2017-10-10 21:11:36 +0200

    .

--- src/main/java/sk/stopangin/validate/Dummy.java
+++ src/main/java/sk/stopangin/validate/Dummy.java
@@ -3,9 +3,6 @@ package sk.stopangin.validate;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 /**
  * Created by stopangin on 10/3/2017.
  */
@@ -15,6 +12,6 @@ public class Dummy {
     @MyValidation("a")
     private String d1;
 
-    @Size(min = 2)
+    @MyValidation("b")
     private String d2;
 }
--- src/main/java/sk/stopangin/validate/MyValidator.java
+++ src/main/java/sk/stopangin/validate/MyValidator.java
@@ -4,14 +4,16 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class MyValidator implements ConstraintValidator<MyValidation, String> {
+   private String val;
+
     @Override
     public void initialize(MyValidation myValidation) {
-
+        val = myValidation.value();
     }
 
     @Override
     public boolean isValid(String s, ConstraintValidatorContext cvc) {
-        if (s.equals("a")) {
+        if (s.equals(val)) {
             return true;
         }
         cvc.buildConstraintViolationWithTemplate("${validatedValue} is not {value}")
